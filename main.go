9b66bf091b18b07bb2ee5d857e757981a3a4c70c
NatchanonLao <nutchanon@digitalcommerce.io>
2022-11-30 15:49:53 +0700

    added redis integration

--- main.go
+++ main.go
@@ -19,11 +19,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"recipes-api/handlers"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -32,6 +34,13 @@ import (
 var recipesHandler *handlers.RecipesHandler
 
 func init() {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	status := redisClient.Ping()
+	fmt.Println(status)
 	ctx := context.Background()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
 	if err = client.Ping(context.TODO(),
@@ -40,7 +49,7 @@ func init() {
 	}
 	log.Println("Connected to MongoDB")
 	collection := client.Database(os.Getenv("MONGO_DATABASE")).Collection("recipes")
-	recipesHandler = handlers.NewRecipesHandler(ctx, collection)
+	recipesHandler = handlers.NewRecipesHandler(ctx, collection, redisClient)
 
 }
 func main() {
